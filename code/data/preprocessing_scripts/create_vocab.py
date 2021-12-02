71134b22b0378ad36cdbfe52aa08159b628ef40f
DMKE-Lab <74903460+DMKE-Lab@users.noreply.github.com>
2021-12-02 18:37:43 +0800

    Update create_vocab.py

--- code/data/preprocessing_scripts/create_vocab.py
+++ code/data/preprocessing_scripts/create_vocab.py
@@ -1,33 +1,6 @@
 import json
 import csv
 import os
-
-root_dir = '../../../'
-vocab_dir = root_dir+'datasets/data_preprocessed/icews/vocab/'
-dir = root_dir+'datasets/data_preprocessed/icews/'
-os.makedirs(vocab_dir)
-entity_vocab = {}
-relation_vocab = {}
-tim_vocab = {}
-action_vocab = {}
-entity_vocab['PAD'] = len(entity_vocab)
-entity_vocab['UNK'] = len(entity_vocab)
-relation_vocab['PAD'] = len(relation_vocab)
-relation_vocab['DUMMY_START_RELATION'] = len(relation_vocab)
-relation_vocab['NO_OP'] = len(relation_vocab)
-relation_vocab['UNK'] = len(relation_vocab)
-tim_vocab['PAD'] = len(tim_vocab)
-tim_vocab['DUMMY_START_TIM'] = len(tim_vocab)
-tim_vocab['NO_OP'] = len(tim_vocab)
-tim_vocab['UNK'] = len(tim_vocab)
-action_vocab['PAD'] = len(action_vocab)
-action_vocab['DUMMY_START_ACTION'] = len(action_vocab)
-action_vocab['NO_OP'] = len(action_vocab)
-action_vocab['UNK'] = len(action_vocab)
-entity_counter = len(entity_vocab)
-relation_counter = len(relation_vocab)
-tim_counter = len(tim_vocab)
-action_counter = len(action_vocab)
 for f in ['train.txt', 'dev.txt', 'test.txt', 'graph.txt']:
     with open(dir+f, encoding='UTF-8') as raw_file:
         csv_file = csv.reader(raw_file, delimiter='\t')
@@ -49,12 +22,3 @@ for f in ['train.txt', 'dev.txt', 'test.txt', 'graph.txt']:
                 if r not in action_vocab:
                     action_vocab[r + ' ' + tim] = action_counter
                     action_counter += 1
-with open(vocab_dir + 'entity_vocab.json', 'w') as fout:
-    json.dump(entity_vocab, fout)
-with open(vocab_dir + 'relation_vocab.json', 'w') as fout:
-    json.dump(relation_vocab, fout)
-with open(vocab_dir + 'tim_vocab.json', 'w') as fout:
-    json.dump(tim_vocab, fout)
-with open(vocab_dir + 'action_vocab.json', 'w') as fout:
-    json.dump(action_vocab, fout)
-
